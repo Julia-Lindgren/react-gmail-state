b4476168829275c2335c07d9c055b2a2e55fde1e
Julia <julia.r.lindgren@gmail.com>
2024-09-29 21:54:04 +0100

    Complete extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,26 @@ function App() {
   // Use initialEmails for state
   const [emails, setEmails] = useState(initialEmails);
 
+  const toggleRead = (targetEmailId) => {
+    setEmails((prevEmails) =>
+      prevEmails.map((email) =>
+        email.id === targetEmailId
+          ? { ...email, read: !email.read } // Toggle read status
+          : email
+      )
+    );
+  };
+
+  const toggleStar = (targetEmailId) => {
+    setEmails((prevEmails) =>
+      prevEmails.map((email) =>
+        email.id === targetEmailId
+          ? { ...email, starred: !email.starred } // Toggle starred status
+          : email
+      )
+    );
+  };
+
   return (
     <div className="app">
       <Header />
@@ -18,14 +38,14 @@ function App() {
             // onClick={() => {}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.filter((email) => !email.read).length}</span>
           </li>
           <li
             className="item"
             // onClick={() => {}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{emails.filter((email) => email.starred).length}</span>
           </li>
 
           <li className="item toggle">
@@ -48,10 +68,10 @@ function App() {
               className={`email ${email.read ? 'read' : 'unread'}`}
             >
               <div className="select">
-                <input type="checkbox" className="select-checkbox" />
+                <input type="checkbox" className="select-checkbox"  checked={email.read} onChange={() => toggleRead(email.id)}/>
               </div>
               <div className="star">
-                <input type="checkbox" className="star-checkbox" checked={email.starred} />
+                <input type="checkbox" className="star-checkbox" checked={email.starred} onChange={() => toggleStar(email.id)} />
               </div>
               <div className="sender">{email.sender}</div>
               <div className="title">{email.title}</div>
